b48e8d5b9cf17e501566264315d0f2d67ab325cc
WV-no7 <beagle.s2513@gmail.com>
2021-12-14 01:54:36 +0530

    Adding room and check-in pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,44 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+/* eslint-disable prettier/prettier */
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+import Rooms from '../views/Rooms.vue';
+import CheckIn from '../views/CheckIn.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+  },
+  {
+    path: '/rooms',
+    name: 'Rooms',
+    component: Rooms,
+  },
+  {
+    path: '/checkin/:hostID/:roomID',
+    name: 'CheckIn',
+    component: CheckIn,
+  },
+  {
+    path: '*',
+    redirect: '/'
+  }
 ];
 
 const router = new VueRouter({
