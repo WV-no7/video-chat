a132ca83e86a2838a87bf2504e7610e500b76950
WV-no7 <beagle.s2513@gmail.com>
2021-12-21 14:57:26 +0530

    updated files

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+/* eslint-disable prettier/prettier */
+<template>
+  <div id="app">
+    <Navigation :user='user' @logout="logout"/>
+    <router-view :user='user' @logout="logout" :rooms="rooms" @addRoom="addRoom" @deleteRoom="deleteRoom" @checkIn="checkIn"/>
+    <Footer />
+  </div>
+</template>
+<script>
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import Navigation from '@/components/Navigation'
+import Footer from '@/components/Footer'
+import db from './db.js'
+
+export default {
+  name:'App',
+  data: function(){
+    return {
+      user: null,
+      rooms:[]
+    }
+  },
+  methods: {
+    logout: function(){
+      firebase.auth().signOut()
+      .then( () => {
+        this.user = null,
+        this.$router.push('login')
+      })
+    },
+    addRoom: function (payload){
+      db.collection('users')
+      .doc(this.user.uid)
+      .collection('rooms')
+      .add({
+        name: payload,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp()
+      })
+    },
+    deleteRoom: function(payload){
+      db.collection('users')
+      .doc(this.user.uid)
+      .collection('rooms')
+      .doc(payload)
+      .delete()
+    },
+    checkIn: function(payload){
+      const roomRef = db.collection('users')
+      .doc(payload.hostID)
+      .collection('rooms')
+      .doc(payload.roomID)
+
+      roomRef.get().then(doc => {
+        if(doc.exists){
+          roomRef.collection('attendees')
+          .doc(this.user.uid)
+          .set({
+            displayName: payload.displayName,
+            approved: payload.hostID == this.user.uid? true : false,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
+          })
+          .then(() => this.$router.push(`/chat/${payload.hostID}/${payload.roomID}`))
+        }
+      })
+    }
+  },
+  mounted() {
+    firebase.auth().onAuthStateChanged( user =>{
+      if(user) {
+        this.user = user
+        db.collection('users')
+        .doc(this.user.uid)
+        .collection('rooms')
+        .onSnapshot(snapshot => {
+          const snapData = []
+          snapshot.forEach(doc => {
+            snapData.push({
+              id: doc.id,
+              name: doc.data().name
+            })
+          })
+          this.rooms = snapData.sort((a,b) =>{
+            if(a.name.toLowerCase() < b.name.toLowerCase()){
+              return -1
+            }else{
+              return 1
+            }
+          })
+        })
+      }
+    })
+  },
+  components:{
+    Navigation,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+$primary: #5f2882;
+@import 'node_modules/bootstrap/scss/bootstrap';
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-light text-muted fixed-bottom">
+        <div class="text-left p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+            <img src="../assets/wvno7.jpg" alt="Avatar" class="avatar">
+            <a class="text-reset fw-bold p-2" href="https://github.com/WV-no7">WV.no7</a>
+        </div>
+    </footer>
+    <!-- Footer -->
+</template>
+<style lang="scss">
+.avatar {
+  vertical-align: middle;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+</style>
