58be5ed84f99199b1f96dee2bc8ad54ad2007720
WV-no7 <beagle.s2513@gmail.com>
2021-12-22 17:24:56 +0530

    minor fixes

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,19 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:WV-no7/video-chat.git main:gh-pages
+# git push -f git@github.com:WV-no7/video-chat.git main:gh-pages
 
-cd -
+# cd -
